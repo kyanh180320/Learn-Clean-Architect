beef8adba9f0541a3471c080908e6f258775da7e
kyanh180320 <kyanh180320@gmail.com>
2026-03-23 17:43:53 +0700

    Update

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    // Domain/Interfaces/IRepository.cs
+    public interface IRepository<T> where T : class
+    {
+        // Read
+        Task<T?> GetByIdAsync(object id, CancellationToken ct = default);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+        Task<List<T>> ListAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default);
+        IQueryable<T> Query();
+
+        // Write
+        Task AddAsync(T entity, CancellationToken ct = default);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken ct = default);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+        // Save
+        Task<int> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    // Infrastructure/Repositories/BaseRepository.cs
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        // Read
+        public async Task<T?> GetByIdAsync(object id, CancellationToken ct = default)
+            => await _dbSet.FindAsync([id], ct);
+
+        public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default)
+            => await _dbSet.AsNoTracking().FirstOrDefaultAsync(predicate, ct);
+
+        public async Task<List<T>> ListAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default)
+            => predicate is null
+                ? await _dbSet.AsNoTracking().ToListAsync(ct)
+                : await _dbSet.AsNoTracking().Where(predicate).ToListAsync(ct);
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default)
+            => await _dbSet.AnyAsync(predicate, ct);
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default)
+            => predicate is null
+                ? await _dbSet.CountAsync(ct)
+                : await _dbSet.CountAsync(predicate, ct);
+
+        public IQueryable<T> Query()
+            => _dbSet.AsNoTracking();
+
+        // Write
+        public async Task AddAsync(T entity, CancellationToken ct = default)
+            => await _dbSet.AddAsync(entity, ct);
+
+        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken ct = default)
+            => await _dbSet.AddRangeAsync(entities, ct);
+
+        public void Update(T entity)
+            => _dbSet.Update(entity);
+
+        public void UpdateRange(IEnumerable<T> entities)
+            => _dbSet.UpdateRange(entities);
+
+        public void Remove(T entity)
+            => _dbSet.Remove(entity);
+
+        public void RemoveRange(IEnumerable<T> entities)
+            => _dbSet.RemoveRange(entities);
+
+        // Save
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+            => await _context.SaveChangesAsync(ct);
+    }
+}
